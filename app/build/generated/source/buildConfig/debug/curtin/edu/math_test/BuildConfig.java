5398c66767968d2ee6951d6ee5863af844edcd15
Franz Labrador <franzstevenlabrador@gmail.com>
2025-01-12 22:12:06 +0800

    upload initial files from 2021

---
+++ app/build/generated/source/buildConfig/debug/curtin/edu/math_test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package curtin.edu.math_test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "curtin.edu.a19819211_assignment_2";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
